35858e4caafc42749769419cd6c147e40b37ea0f
Yan Razvadovskyi <116461898+yanrzv@users.noreply.github.com>
2023-03-18 03:08:21 +0200

    ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,15 @@
-import Vimeo from "@vimeo/player";
-
-
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
 
-console.log(Vimeo.Player);
+player.on('timeupdate', throttle(getTime, 1000));
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
-
-
-
-
-// import Player from '@vimeo/player';
-
-// const player = new Player('handstick', {
-//     id: 'vimeo-player',
-//     width: 640
-// });
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+function getTime() {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem(STORAGE_KEY, seconds);
+    })
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector('form');
+const email = form.querySelector('input[name="email"]');
+const message = form.querySelector('textarea');
+let localStorageObject = {};
+let storageValue = {};
+
+form.addEventListener('input', throttle(formInput, 500)); 
+
+
+function formInput (e) {
+    localStorageObject[e.target.name] = e.target.value;
+
+    storageValue = JSON.stringify(localStorageObject);
+    
+    localStorage.setItem(STORAGE_KEY, storageValue)
+
+}
+
+const formData = localStorage.getItem(STORAGE_KEY)
+
+if (formData) {
+    const parsedLocalStorage = JSON.parse(formData);
+    
+    if (parsedLocalStorage.message) {
+    message.value = parsedLocalStorage.message;
+    } 
+    
+    if (parsedLocalStorage.email) {
+    email.value = parsedLocalStorage.email;
+    };
+}
+
+    
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    if(email.value.length > 1 && message.value.length > 1) {
+        console.log(localStorageObject);
+
+        form.reset();
+        localStorage.removeItem('feedback-form-state');
+    } else {
+        alert(`Заповніть всі поля`)
+    }    
+} )
+
+
+
 
